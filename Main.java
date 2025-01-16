af55a231af11dc3e8d02085a779fc712d17baaa6
Almazbek Bazarbekov <yaklass.312@gmail.com>
2025-01-16 21:56:47 +0600

    Display Board

--- Main.java
+++ Main.java
@@ -14,6 +14,10 @@ public class Main {
             String[][] hiddenBoard = new String[7][7];
             String[][] playerBoard = new String[7][7];
             initializeBoards(hiddenBoard,playerBoard);
+            int shots = 0;
+            int hits = 0;
+            int totalShipParts = 11;
+            
 
 
         }
@@ -22,6 +26,16 @@ public class Main {
         for (int i = 0; i < 7; i++) {
             Arrays.fill(hiddenBoard[i], "🌊");
             Arrays.fill(playerBoard[i], "🌊");
+        }
+    }
+    static void displayBoard(String[][] board) {
+        System.out.print("  1 2 3 4 5 6 7\n");
+        for (int i = 0; i < 7; i++) {
+            System.out.print((char) ('A' + i) + " ");
+            for (int j = 0; j < 7; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }

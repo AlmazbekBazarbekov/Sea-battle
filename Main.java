eb3d8287528e060261a69decdd81aef7e3809b0d
Almazbek Bazarbekov <yaklass.312@gmail.com>
2025-01-19 20:38:07 +0600

    second part of ship placement

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -11,97 +12,152 @@ public class Main {
             System.out.print("Enter your name: ");
             String playerName = scanner.nextLine();
             playerNames.add(playerName);
+
             String[][] hiddenBoard = new String[7][7];
             String[][] playerBoard = new String[7][7];
-            initializeBoards(hiddenBoard,playerBoard);
-            int shots = 0;
-            int hits = 0;
+            initializeBoards(hiddenBoard, playerBoard);
+
+            placeShips(hiddenBoard);
+
+            int shotsTaken = 0;
+            int hitsCount = 0;
             int totalShipParts = 11;
 
-            while (hits < totalShipParts) {
+            while (hitsCount < totalShipParts) {
                 displayBoard(playerBoard);
                 System.out.print("Enter your shot (e.g., B3): ");
-                String input = scanner.nextLine().toUpperCase();
-                if (!isValidShot(input)) {
+                String shotInput = scanner.nextLine().toUpperCase();
+
+                if (!isValidShot(shotInput)) {
                     System.out.println("Invalid input. Please try again.");
                     continue;
                 }
 
-                int row = input.charAt(0) - 'A';
-                int col = input.charAt(1) - '1';
+                int shotRow = shotInput.charAt(0) - 'A';
+                int shotCol = shotInput.charAt(1) - '1';
 
-                if (!playerBoard[row][col].equals("🌊")) {
+                if (!playerBoard[shotRow][shotCol].equals("🌊")) {
                     System.out.println("You have already shot here. Please try again.");
                     continue;
                 }
 
-                shots++;
-                if (hiddenBoard[row][col].equals("🚢")) {
-                    playerBoard[row][col] = "🔥";
-                    hiddenBoard[row][col] = "X";
-                    hits++;
+                shotsTaken++;
+                if (hiddenBoard[shotRow][shotCol].equals("🚢")) {
+                    playerBoard[shotRow][shotCol] = "🔥";
+                    hiddenBoard[shotRow][shotCol] = "X";
+                    hitsCount++;
                     System.out.println("Hit!");
-                } 
-                else {
-                    playerBoard[row][col] = "❌";
+                } else {
+                    playerBoard[shotRow][shotCol] = "❌";
                     System.out.println("Miss!");
                 }
-            System.out.println("Congratulations, " + playerName + "! You won in " + shots + " shots.");
-            playerScores.add(shots);
-    
+            }
+
+            System.out.println("Congratulations, " + playerName + "! You won in " + shotsTaken + " shots.");
+            playerScores.add(shotsTaken);
+
             System.out.print("Do you want to play again? (yes/no): ");
             if (!scanner.nextLine().equalsIgnoreCase("yes")) break;
-            }
-            System.out.println("Final Scores:");
-            List<Integer> sortedScores = new ArrayList<>(playerScores);
-            Collections.sort(sortedScores);
-            for (int i = 0; i < sortedScores.size(); i++) {
-                int score = sortedScores.get(i);
-                int index = playerScores.indexOf(score);
-                System.out.println(playerNames.get(index) + ": " + score + " shots");
-            }
-            scanner.close();
         }
+
+        System.out.println("Final Scores:");
+        List<Integer> sortedScores = new ArrayList<>(playerScores);
+        Collections.sort(sortedScores);
+        for (int i = 0; i < sortedScores.size(); i++) {
+            int score = sortedScores.get(i);
+            int index = playerScores.indexOf(score);
+            System.out.println(playerNames.get(index) + ": " + score + " shots");
+        }
+
+        scanner.close();
     }
 
     static void initializeBoards(String[][] hiddenBoard, String[][] playerBoard) {
-        for (int i = 0; i < 7; i++) {
-            Arrays.fill(hiddenBoard[i], "🌊");
-            Arrays.fill(playerBoard[i], "🌊");
+        for (int row = 0; row < 7; row++) {
+            Arrays.fill(hiddenBoard[row], "🌊");
+            Arrays.fill(playerBoard[row], "🌊");
         }
     }
-    static void displayBoard(String[][] board) {
-        System.out.print("  1 2 3 4 5 6 7\n");
-        for (int i = 0; i < 7; i++) {
-            System.out.print((char) ('A' + i) + " ");
-            for (int j = 0; j < 7; j++) {
-                System.out.print(board[i][j] + " ");
+
+    static void placeShips(String[][] board) {
+        placeShip(board, 3); 
+        placeShip(board, 2); 
+        placeShip(board, 2);
+        placeShip(board, 1); 
+        placeShip(board, 1);
+        placeShip(board, 1);
+        placeShip(board, 1);
+    }
+
+    static void placeShip(String[][] board, int shipSize) {
+        Random random = new Random();
+        boolean shipPlaced = false;
+
+        while (!shipPlaced) {
+            int startRow = random.nextInt(7);
+            int startCol = random.nextInt(7);
+            boolean isHorizontal = random.nextBoolean();
+
+            if (canPlaceShip(board, startRow, startCol, shipSize, isHorizontal)) {
+                for (int part = 0; part < shipSize; part++) {
+                    if (isHorizontal) {
+                        board[startRow][startCol + part] = "🚢";
+                    } else {
+                        board[startRow + part][startCol] = "🚢";
+                    }
+                }
+                shipPlaced = true;
             }
-            System.out.println();
         }
     }
+
+    static boolean canPlaceShip(String[][] board, int row, int col, int size, boolean horizontal) {
+        for (int part = 0; part < size; part++) {
+            int currentRow;
+            int currentCol;
+
+            if (horizontal) {
+                currentRow = row;
+                currentCol = col + part;
+            } else {
+                currentRow = row + part;
+                currentCol = col;
+            }
+
+            if (currentRow < 0 || currentRow >= 7 || currentCol < 0 || currentCol >= 7 || !board[currentRow][currentCol].equals("🌊")) {
+                return false;
+            }
+
+            for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+                for (int colOffset = -1; colOffset <= 1; colOffset++) {
+                    int adjacentRow = currentRow + rowOffset;
+                    int adjacentCol = currentCol + colOffset;
+
+                    if (adjacentRow >= 0 && adjacentRow < 7 && adjacentCol >= 0 && adjacentCol < 7 && board[adjacentRow][adjacentCol].equals("🚢")) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
     static boolean isValidShot(String input) {
         if (input.length() != 2) return false;
         char row = input.charAt(0);
         char col = input.charAt(1);
         return row >= 'A' && row <= 'G' && col >= '1' && col <= '7';
     }
-    static void placeShips(String[][] board) {
-        placeShip(board, 3);
-        placeShip(board, 2);
-        placeShip(board, 2);
-        placeShip(board, 1);
-        placeShip(board, 1);
-        placeShip(board, 1);
-        placeShip(board, 1);
-    }
-    static void placeShip(String[][] board, int size) {
-        Random random = new Random();
-        boolean placed = false;
-        while (!placed) {
-            int row = random.nextInt(7);
-            int col = random.nextInt(7);
-            boolean horizontal = random.nextBoolean();
+
+    static void displayBoard(String[][] board) {
+        System.out.print("  1 2 3 4 5 6 7\n");
+        for (int row = 0; row < 7; row++) {
+            System.out.print((char) ('A' + row) + " ");
+            for (int col = 0; col < 7; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
         }
     }
 }
+
